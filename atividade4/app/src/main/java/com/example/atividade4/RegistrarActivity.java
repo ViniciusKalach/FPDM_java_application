9cdf767d0aefd910ca4b503061ad8b42d78ed2f2
Vinícius Kalach <112291145+ViniciusKalach@users.noreply.github.com>
2024-05-06 20:39:07 -0300

    Refactor and fix Atividade4

--- atividade4/app/src/main/java/com/example/atividade4/RegistrarActivity.java
+++ atividade4/app/src/main/java/com/example/atividade4/RegistrarActivity.java
@@ -1,54 +1,77 @@
 package com.example.atividade4;
 
-import android.os.Bundle;
-
-import com.example.atividade4.R;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.atividade4.databinding.ActivityRegistrarBinding;
+import com.example.atividade4.ListaUsuarioActivity;
+import com.example.atividade4.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrarActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityRegistrarBinding binding;
-
+    EditText editEmail, editSenha;
+    Button buttonSalvar;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityRegistrarBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_registrar);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        buttonSalvar = (Button) findViewById(R.id.buttonSalvar);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        mAuth = FirebaseAuth.getInstance();
+        buttonSalvar.setOnClickListener(v -> inserirUsuario());
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent i = new Intent(getApplicationContext(),
+                    ListaUsuarioActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void inserirUsuario(){
+        if(TextUtils.isEmpty(editEmail.getText())){
+            Toast.makeText(this, "E-mail obrigatório",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(editSenha.getText())){
+            Toast.makeText(this, "Senha obrigatória",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(
+                editEmail.getText().toString(),
+                editSenha.getText().toString()
+        ).addOnCompleteListener(task -> {
+            progressBar.setVisibility(View.VISIBLE);
+            if(task.isSuccessful()){
+                Toast.makeText(this, "Salvo com sucesso!",
+                        Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(),
+                        LoginActivity.class);
+                startActivity(i);
+                finish();
+            }else{
+                Toast.makeText(this, "Falha ao salvar",
+                        Toast.LENGTH_SHORT).show();
+            }
+            progressBar.setVisibility(View.GONE);
+        });
     }
 }
